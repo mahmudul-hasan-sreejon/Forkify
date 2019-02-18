ae0c5b884015420eafa0d23f79fccb426e281118
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-02-18 14:22:37 +0600

    Search controller added

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,35 @@
 
 import Search from './models/Search';
 
-const search = new Search('pizza');
 
-console.log(search);
-search.getResults();
+// Global state of the app
+// - Search object
+// - Current recipe object
+// - Shopping list object
+// - Liked object
+const state = {};
+
+
+const controlSearch = async () => {
+    // Get query form view
+    const query = 'pizza';
+
+    if(query) {
+        // Create new Search object and add to the state
+        state.search = new Search(query);
+
+        // Prepare UI for results
+
+        // Search for recipes
+        await state.search.getResults();
+
+        // Render results on UI
+        console.log(state.search.result);
+    }
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+
+    controlSearch();
+});
