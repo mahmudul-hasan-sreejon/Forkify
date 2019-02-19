acce360f65b38fa064f70d13af23cdd18ec0cb82
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-02-19 19:20:10 +0600

    Added limitRecipeTitle method in the search view
    Limited recipe titles to 17 chars

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -14,6 +14,21 @@ export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 };
 
+// Limit Recipe Title view chars to 17 chars
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if(title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if(acc + cur.length <= limit) newTitle.push(cur);
+
+            return (acc + cur.length);
+        }, 0);
+
+        return (`${newTitle.join(' ')} ...`);
+    }
+    return title;
+};
+
 // Render recipe results on the UI
 const renderRecipe = recipe => {
     const markup = `
@@ -23,7 +38,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
