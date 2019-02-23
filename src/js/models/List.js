d06cd8ddd6db0d7dd9bdb63d599a8b496dd1770c
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-02-23 16:09:15 +0600

    Shopping list model added

---
+++ src/js/models/List.js
@@ -0,0 +1,36 @@
+
+import uniqid from 'uniqid';
+
+
+export default class List {
+    constructor() {
+        this.items = [];
+    }
+
+    // Add an item
+    addItem(count, unit, ingredient) {
+        const item = {
+            id: uniqid,
+            count,
+            unit,
+            ingredient
+        };
+
+        // Add the new item
+        this.items.push(item);
+    }
+
+    // Delete an item
+    deleteItem(id) {
+        // Find the index of the item with the given `id`
+        const index = this.items.findIndex(el => el.id === id);
+        // Delete item
+        this.items.splice(index, 1);
+    }
+
+    // Update ingredient count
+    updateCount(id, newCount) {
+        // Find item with the given `id` and update it with the new count
+        this.items.find(el => el.id === id).count = newCount;
+    }
+}
