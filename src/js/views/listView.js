a4c1cbb6d093d43997db4edbb932dc15847292ef
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-02-23 16:26:57 +0600

    Shopping list view added

---
+++ src/js/views/listView.js
@@ -0,0 +1,35 @@
+
+import { elements } from './base';
+
+
+// Render list item(s)
+export const renderItem = item => {
+    const markup = `
+        <li class="shopping__item" data-itemid=${item.id}>
+            <div class="shopping__count">
+                <input type="number" value="${item.count}" step="${item.count}" class="shopping__count-value">
+                <p>${item.unit}</p>
+            </div>
+
+            <p class="shopping__description">${item.ingredient}</p>
+
+            <button class="shopping__delete btn-tiny">
+                <svg>
+                    <use href="img/icons.svg#icon-circle-with-cross"></use>
+                </svg>
+            </button>
+        </li>
+    `;
+
+    // Add list item(s) in the UI
+    elements.shopping.insertAdjacentHTML('beforeend', markup);
+};
+
+// Delete list item(s)
+export const deleteItem = id => {
+    // Find the item of given `id`
+    const item = document.querySelector(`[data-itemid="${id}"]`);
+
+    // Remove the item
+    item.parentElement.removeChild(item);
+};
